837c7d0f100abec3d7f5b1f5dbefd5d0ba211d35
newbie-citizen <newbie.citizen@gmail.com>
2024-06-15 13:55:55 +0700

    .

--- plugin.js
+++ plugin.js
@@ -6,7 +6,7 @@ lib.hash.require (MD5, SHA256, SHA1);
 var {pathToRegexp, match, parse, compile} = lib.path.regex.require ();
 lib.path.regex.require (pathToRegexp, match, parse, compile);
 
-var {Client, Databases, Query, ID} = lib.api.appwrite.require ();
-lib.api.appwrite.require (Client, Databases, Query, ID);
+var {Client, Databases, Query, ID} = lib.appwrite.require ();
+lib.appwrite.require (Client, Databases, Query, ID);
 
 module.exports = exports = lib;
